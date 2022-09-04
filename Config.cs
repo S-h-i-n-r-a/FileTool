639ae00c17fe489e74b60092cc4c2a99871981c4
Shinra <93828376+S-h-i-n-r-a@users.noreply.github.com>
2022-09-04 02:39:49 +0200

    created

---
+++ Config.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// TODO: messy
+
+namespace FileTool
+{
+    internal static class Config
+    {
+        private static string execDir;
+        internal static bool failed;
+
+        // Directories
+        internal static string? srcDir;
+        internal static string? tgtDir;
+
+        // File Modifications
+        internal static string? tgtFileT;
+        internal static string? prefix;
+        internal static string? suffix;
+
+        // Repeat Operations
+        internal static bool repeat;
+        internal static int pauseMillis;
+
+        static Config()
+        {
+            execDir = AppDomain.CurrentDomain.BaseDirectory;
+            failed = false;
+            
+            if (!File.Exists(execDir + "\\FileTool.cfg")) { Console.WriteLine("ERROR: no config file found."); failed = true; return; }
+
+
+            // Read cfg file
+            StreamReader sr = new(execDir + "\\FileTool.cfg");
+
+            sr.ReadLine();
+            srcDir = sr.ReadLine();
+            if (srcDir == null || srcDir == "\"\"") { Console.WriteLine("ERROR: source directory cannot be empty."); failed = true; return; }
+            srcDir = srcDir.Replace("\"", "");
+
+            sr.ReadLine(); sr.ReadLine();
+            tgtDir = sr.ReadLine();
+            if (tgtDir == null || tgtDir == "\"\"") { Console.WriteLine("ERROR: target directory cannot be empty."); failed = true; return; }
+            tgtDir = tgtDir.Replace("\"", "");
+
+            sr.ReadLine(); sr.ReadLine();
+            tgtFileT = sr.ReadLine();
+            if (tgtFileT != null) { tgtFileT = tgtFileT.Replace("\"", ""); }
+            else { tgtFileT = ".*"; }
+
+            sr.ReadLine(); sr.ReadLine();
+            prefix = sr.ReadLine();
+            if (prefix != null) { prefix = prefix.Replace("\"", ""); }
+
+            sr.ReadLine(); sr.ReadLine();
+            suffix = sr.ReadLine();
+            if (suffix != null) { suffix = suffix.Replace("\"", ""); }
+
+            sr.ReadLine(); sr.ReadLine();
+            if (!bool.TryParse(sr.ReadLine(), out repeat)) 
+            { Console.WriteLine("WARNING: Invalid value @ repeat, fallback to false."); repeat = false; }
+
+            sr.ReadLine(); sr.ReadLine();
+            if (!int.TryParse(sr.ReadLine(), out pauseMillis))
+            { Console.WriteLine("WARNING: Invalid value @ interval, fallback to 10 minutes."); pauseMillis = 600000; }
+            else { pauseMillis *= 1000; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace FileTool
+{
+    internal class Program
+    {
+        private const string authLogo =
+            "██████████████████████████████████████████████████\n" +
+            "██░░░░░░██░░██░░██░░░░░░██░░░░░░██░░░░░░██░░░░░░██\n" +
+            "██░░██████░░██░░████░░████░░██░░██░░██░░██░░██░░██\n" +
+            "██░░░░░░██░░░░░░████░░████░░██░░██░░░░████░░░░░░██\n" +
+            "██████░░██░░██░░████░░████░░██░░██░░██░░██░░██░░██\n" +
+            "██░░░░░░██░░██░░██░░░░░░██░░██░░██░░██░░██░░██░░██\n" +
+            "██████████████████████████████████████████████████\n" +
+            "██░░░░░░░░░░░ github.com/S-h-i-n-r-a ░░░░░░░░░░░██\n" +
+            "██████████████████████████████████████████████████";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(authLogo + "\n\nFileTool starting..");
+            
+            // Calling Config.failed automatically loads the config
+            if (Config.failed) { Console.WriteLine("Failed to read config, check the file and restart.\nPress enter to exit."); Console.ReadLine(); return; }
+
+            Console.WriteLine("\nDone!\n");
+
+            if (Config.repeat)
+            {
+                Console.WriteLine("Running in repeat mode..\nEnter \"exit\" command at any time to quit.");
+                Thread thread = new(RepeatManipFiles);
+                thread.Start();
+
+                string? command = "";
+                while (!command.Equals("exit")) { command = Console.ReadLine() ?? ""; }
+                thread.Interrupt();
+                while (thread.IsAlive) { /* Wait for thread to exit */ }
+            }
+            else
+            {
+                ManipulateFiles();
+                Console.WriteLine("Done!");
+            }
+
+            Console.WriteLine("Exiting..");
+        }
+
+
+#pragma warning disable CS8604  // Checks are performed in Config, null values never reach this section
+        private static void ManipulateFiles()
+        {
+            Console.WriteLine("\nLooking for files..\n");
+            string[] files = Directory.GetFiles(Config.srcDir, Config.tgtFileT);
+            if (files.Length == 0) { Console.WriteLine("No new files found.\n"); return; }
+            for (int i = 0; i < files.Length; ++i) { Console.WriteLine(files[i]); }
+
+            Console.WriteLine("\nApplying transformations..\n");
+            if (!Directory.Exists(Config.tgtDir)) { Directory.CreateDirectory(Config.tgtDir); }
+            for (int i = 0; i < files.Length; ++i)
+            {
+                string filename = files[i].Substring(files[i].LastIndexOf('\\') +1, files[i].LastIndexOf('.') - files[i].LastIndexOf('\\') -1);
+                string filetype = files[i].Substring(files[i].LastIndexOf('.') +1);
+
+                string transformedFile = $"{Config.tgtDir}\\{Config.prefix + filename + Config.suffix}.{filetype}";
+                try 
+                { 
+                    File.Copy(files[i], transformedFile);
+                    File.Delete(files[i]);
+                    Console.WriteLine(transformedFile); 
+                }
+                catch (Exception e) { Console.WriteLine($"ERROR: {e}"); }
+            }
+            Console.WriteLine();
+        }
+#pragma warning restore CS8604
+
+        private static void RepeatManipFiles()
+        {
+            ManipulateFiles();
+            Console.WriteLine($"Waiting for {Config.pauseMillis / 1000} seconds..\n");
+            try { Thread.Sleep(Config.pauseMillis); }
+            catch { Console.WriteLine("INFO: Thread interrupted while in SLEEP state."); return; }
+            RepeatManipFiles();
+        }
+    }
+}
